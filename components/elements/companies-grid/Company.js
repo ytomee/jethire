54385ed490c137921842197db41983d5820a8850
Tomé Almeida <121977238+ytomee@users.noreply.github.com>
2025-05-26 20:06:12 +0100

    company error

--- components/elements/companies-grid/Company.js
+++ components/elements/companies-grid/Company.js
@@ -1,241 +1,107 @@
+// pages/company/index.tsx (ou .jsx)
 import Link from "next/link";
+import connectMongoDB from "../../../lib/mongodb";
+import Company from "../../../models/company";
+console.log("Modelo Company:", Company);
 
-export default function Company() {
+export default function CompanyPage({ companies = [] }) {
+
+    if (!companies || companies.length === 0) {
+        return <p className="text-center py-5">Nenhuma empresa encontrada.</p>;
+    }
+
     return (
         <div className="row">
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-1.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Car Toys</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>66</span>
-                                <span>)</span>
-                            </span>
+            {companies.map((company) => (
+                <div key={company._id} className="col-md-6 col-sm-12 col-12 mb-4">
+                    <div className="card-grid-3 d-flex flex-column h-100">
+                        <div className="position-relative">
+                            <div className="card-grid-3-image">
+                                <Link legacyBehavior href={`/profile/${company._id}`}>
+                                    <a>
+                                        <figure className="mb-0">
+                                            <img
+                                                src={company.banner || "/assets/imgs/default/banner.jpg"}
+                                                alt="Banner"
+                                            />
+                                        </figure>
+                                    </a>
+                                </Link>
+                            </div>
+                            <div className="card-grid-3-pfp position-absolute">
+                                <img
+                                    src={company.pfp || "/assets/imgs/default/user.png"}
+                                    style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                                    alt="Logo"
+                                />
+                            </div>
                         </div>
-                        <span className="card-location">New York, US</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>12 Jobs Open</span>
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-2.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Carols Daughter</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>18</span>
-                                <span>)</span>
-                            </span>
-                        </div>
-                        <span className="card-location">London, UK</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>25</span>
-                                    <span> Jobs Open</span>
-                                </a>
-                            </Link>
+
+                        <div className="card-block-info d-flex flex-column h-100">
+                            <div>
+                                <h5 className="mb-2 pt-2">
+                                    <Link legacyBehavior href={`/profile/${company._id}`}>
+                                        <a>{company.name || "Nome não disponível"}</a>
+                                    </Link>
+                                </h5>
+                                <p className="color-text-paragraph font-sm mb-3">
+                                    {company.descriptions?.[0]?.description || "Sem descrição disponível."}
+                                </p>
+
+                                <div className="skills-wrapper mb-3">
+                                    {company.tags?.slice(0, 4).map((tag, i) => (
+                                        <Link legacyBehavior key={i} href="/jobs-grid">
+                                            <a className="btn btn-tags-sm">{tag}</a>
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className="row mt-auto">
+                                <div className="col-7 d-flex align-items-end">
+                                    <span className="city-info">
+                                        {company.city || "Cidade não definida"}
+                                        {company.country && `, ${company.country}`}
+                                    </span>
+                                </div>
+                                <div className="col-5">
+                                    <span className="d-flex justify-content-end align-items-center">
+                                        <a
+                                            className="btn btn-apply-now"
+                                            href={`/profile/${company._id}`}
+                                            style={{ width: "100%" }}
+                                        >
+                                            Ver perfil
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-3.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Amazon</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>52</span>
-                                <span>)</span>
-                            </span>
-                        </div>
-                        <span className="card-location">Tokyo,Japan</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>54</span>
-                                    <span> Jobs Open</span>
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-4.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Baseball Savings</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>85</span>
-                                <span>)</span>
-                            </span>
-                        </div>
-                        <span className="card-location">Chicago, US</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>6</span>
-                                    <span> Jobs Open</span>
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-5.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Ashford</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>25</span>
-                                <span>)</span>
-                            </span>
-                        </div>
-                        <span className="card-location">Toronto, Italia</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>67</span>
-                                    <span> Jobs Open</span>
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                <div className="card-grid-1 hover-up wow animate__animated animate__fadeIn">
-                    <div className="image-box">
-                        <Link legacyBehavior href="/company-details">
-                            <a>
-                                <img src="assets/imgs/brands/brand-6.png" alt="jobBox" />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="info-text mt-10">
-                        <h5 className="font-bold">
-                            <Link legacyBehavior href="/company-details">
-                                <a>Callaway Golf</a>
-                            </Link>
-                        </h5>
-                        <div className="mt-5">
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <img alt="jobBox" src="assets/imgs/template/icons/star.svg" />
-                            <span className="font-xs color-text-mutted ml-10">
-                                <span>(</span>
-                                <span>34</span>
-                                <span>)</span>
-                            </span>
-                        </div>
-                        <span className="card-location">San Francisco, US</span>
-                        <div className="mt-30">
-                            <Link legacyBehavior href="/jobs-grid">
-                                <a className="btn btn-grey-big">
-                                    <span>45</span>
-                                    <span> Jobs Open</span>
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
-  );
+    );
+}
+
+export async function getServerSideProps() {
+  try {
+    await connectMongoDB();
+    const companies = await Company.find({})
+      .select("-team -pending")
+      .lean();
+
+    return {
+      props: {
+        companies: JSON.parse(JSON.stringify(companies)),
+      },
+    };
+  } catch (error) {
+    console.error("Erro ao buscar empresas:", error);
+    return {
+      props: {
+        companies: [],
+      },
+    };
+  }
 }
--- models/company.js
+++ models/company.js
@@ -1,34 +1,115 @@
 import mongoose, { Schema } from "mongoose";
 
-const descriptionSchema = new Schema({
-  title: String,
-  content: String
-}, { _id: false });
+const descriptionSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+  },
+  { _id: false }
+);
 
-const companySchema = new Schema({
-  type: { type: String, default: 'company' },
-  email: String,
-  password: String,
-  banner: String,
-  pfp: String,
-  name: { type: String, required: true },
-  slogan: String,
-  city: String,
-  country: String,
-  shortDesc: String,
+const userSchema = new mongoose.Schema(
+  {
+    user: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['admin', 'manager', 'recruiter'],
+      default: 'recruiter',
+      required: true,
+    },
+    pfp: { type: String },
+    pfp_id: { type: String },  
+    city: { type: String },
+    country: { type: String },
+    phone: { type: String },
+    socials: [{
+      platform: String,
+      url: String
+    }],
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { _id: true }
+);
 
-  description: [descriptionSchema],
+const pendingSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
+    role: {
+      type: String,
+      enum: ['admin', 'manager', 'recruiter'],
+      default: 'recruiter',
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'pending'],
+      default: 'pending',
+      required: true,
+    },
+    token: { type: String, required: true },
+    expiresAt: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
-  field: String,
-  workType: String,
+const companySchema = new Schema(
+  {
+    type: { type: String, default: "company" },
 
-  contact: {
-    email: String,
+    name: { type: String, required: true },
+    slogan: String,
+
+    city: String,
+    country: String,
     address: String,
-    phone: String
-  }
-}, 
-{ timestamps: true });
 
-const Company = mongoose.models.Company || mongoose.model("Company", companySchema);
+    shortDesc: String,
+    description: [descriptionSchema],
+
+    field: String, 
+    workType: String, 
+    foundationYear: Number,
+
+    tags: { type: [String], default: [] },
+    remote: { type: String, default: "" },
+
+    banner: {
+      secure_url: String,
+      public_id: String,
+    },
+
+    logo: {
+      secure_url: String,
+      public_id: String,
+    },
+
+    contact: {
+      email: String,
+      phone: String,
+      website: String,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    team: [userSchema],
+    pending: [pendingSchema],
+
+  },
+  { timestamps: true }
+);
+
+const Company = 
+  mongoose.models.Company || 
+  mongoose.model("Company", companySchema);
+
 export default Company;
